87f46079f182560f10d7236873451d535044fafa
Shou <nmmarzano@users.noreply.github.com>
2020-02-06 15:42:20 -0300

    use print.js

--- src/index.js
+++ src/index.js
@@ -3,17 +3,23 @@ import './style.scss';
 import Icon from './logo_bp.ico';
 import XmlData from './data.xml';
 import CsvData from './data.csv';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
     const myIcon = new Image();
     myIcon.src = Icon;
 
     element.appendChild(myIcon);
+    element.appendChild(btn);
 
     console.log('XmlData', XmlData);
     console.log('CsvData', CsvData);
